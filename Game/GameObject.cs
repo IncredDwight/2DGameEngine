514629cf1d18e2696245306618b44b85bdbf05bc
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-12-07 13:17:50 -0500

    GameObject

---
+++ Game/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Game
+{
+	public class GameObject
+	{
+		public Vector2 Position;
+		public char View;
+
+		public GameObject()
+		{
+			Position = new Vector2(0, 0);
+			View = ' '; 
+		}
+
+		public GameObject(int x, int y, char view)
+		{
+			Position = new Vector2(x, y);
+			View = view;
+		}
+	}
+}
+
---
+++ Game/Vector2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Game
+{
+	public class Vector2
+	{
+		public int X;
+		public int Y;
+
+		public Vector2()
+		{
+			X = 0;
+			Y = 0;
+		}
+
+		public Vector2(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
+
